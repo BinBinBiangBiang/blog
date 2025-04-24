904fb9dc85129899bf5549ced1d081a6aa234b2d
huangbin094 <huangbin094@hellobike.com>
2025-04-25 00:53:36 +0800

    feat: 新增压缩工具

--- lib/routers.ts
+++ lib/routers.ts
@@ -13,5 +13,10 @@ export const routerList = [
     path: '/guestbook',
     name: '留言板',
     icon: 'mynaui:message-dots'
+  },
+  {
+    path: '/onlineTools/imageZip',
+    name: '压缩工具',
+    icon: 'tabler:file-zip'
   }
 ]
